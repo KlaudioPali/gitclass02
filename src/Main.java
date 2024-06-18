f94090f620232b9b2c8c3d2d297cf4a9fed07e07
Klaudio <klaudio.pali92@gmail.com>
2024-06-17 20:11:07 -0400

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello sdet batch16");
+        System.out.println("hello sdettt batch16");
 
     }
 }
